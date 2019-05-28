7c4c48999e0921975f8d17d6a7d067677d98d6c7
Suboh, Saleh <subohsaleh@gmail.com>
2019-05-28 16:13:04 -0400

    initial commit of project

---
+++ app.js
@@ -0,0 +1,14 @@
+var budgetController = (function() {
+
+})();
+
+
+var UIController = (function() {
+    // some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+  
+
+})(budgetController, UIController);
