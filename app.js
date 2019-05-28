44824118749d9d45991ed7b78fbd9570c01133dc
Suboh, Saleh <subohsaleh@gmail.com>
2019-05-28 16:46:27 -0400

    added some event listeners

--- app.js
+++ app.js
@@ -9,16 +9,23 @@ var UIController = (function() {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var ctrlAddItem = function () {
         // get field input data
         // add item to the budget controller
         // add the item to the UI
         // calculate the budget
         // display the budget on the UI
-    });
+        console.log('called');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-        console.log(event);
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
     });
 
 })(budgetController, UIController);
